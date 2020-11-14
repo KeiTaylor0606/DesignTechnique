a540d2102b6b06fdeb04d741795cce240266c041
KeiTaylor0606 <mvemtae5757@outlook.jp>
2020-11-15 01:15:34 +0900

    1からN までの総和を計算する再帰関数

---
+++ 4.1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i = 0; i < (n); i++)
+using ll = long long;
+using namespace std;
+using P = pair<int,int>;
+
+int func(int N){
+    if(N == 0) return 0;
+    return N + func(N - 1);
+}
