95cbc3dff73541f1d1de161f1ab1cab46d44d6e9
KeiTaylor0606 <mvemtae5757@outlook.jp>
2020-11-15 22:43:55 +0900

    緩和処理を実現するための関数chmin

---
+++ 5.2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i = 0; i < (n); i++)
+using ll = long long;
+using namespace std;
+using P = pair<int,int>;
+
+template<class T> void chmin(T& a, T b){
+    if(a > b){
+        a = b;
+    }
+}
